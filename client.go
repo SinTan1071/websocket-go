e9e6b5bfb3826f71add269b9631ae1d72a4583cb
RT7oney <297085213@qq.com>
2017-06-30 10:46:32 +0800

    debug

--- client.go
+++ client.go
@@ -123,9 +123,10 @@ func serveWsServer(serv *Server, w http.ResponseWriter, r *http.Request) {
 	_msg := r.Form.Get("Message")
 	log.NewLog("client.go-124:读取服务器消息", _msg)
 	decode, _ := base64.StdEncoding.DecodeString(_msg)
+	log.NewLog("client.go-126:读取服务器消息base64解码后的消息", string(decode))
 	var req Request
 	err2 := json.Unmarshal(decode, &req)
-	log.NewLog("client.go-128:读取服务器消息解析后的", req)
+	log.NewLog("client.go-129:读取服务器消息解析后的", req)
 	// fmt.Println("请求", r)
 	// fmt.Println("请求体", r.Body)
 	// target := []string{"0cc120124175285f80dc4b13e18730bb", "858803d74ab00f41c548f11ca4468df1"}
