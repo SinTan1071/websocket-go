84a6bbecb2c79aadcad09ba2d43f4b78d0cc7fc1
RT7oney <297085213@qq.com>
2017-07-03 19:49:40 +0800

    调试

--- conf/config.go
+++ conf/config.go
@@ -3,7 +3,7 @@ package conf
 const PORT = ":12345"
 
 const SERVER_TOKEN = "xxx1qazxsw23edcvfr45tgbnhy67ujmxxx"
-const SERVER_IP = "10.65.209.164"
+const SERVER_IP = "172.20.0.2"
 
 const CLIENT_AUTH = "http://10.65.209.164:8000/api/kong/auth"
 
