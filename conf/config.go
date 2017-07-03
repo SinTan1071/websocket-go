788bb627cd2023c9d351e3f170b0c87fff518534
RT7oney <297085213@qq.com>
2017-07-03 18:19:37 +0800

    修改服务器皮遏制

--- conf/config.go
+++ conf/config.go
@@ -3,8 +3,8 @@ package conf
 const PORT = ":12345"
 
 const SERVER_TOKEN = "xxx1qazxsw23edcvfr45tgbnhy67ujmxxx"
-const SERVER_IP = "192.168.3.4"
+const SERVER_IP = "10.65.209.164"
 
-const CLIENT_AUTH = "http://192.168.3.4:8000/api/kong/auth"
+const CLIENT_AUTH = "http://10.65.209.164:8000/api/kong/auth"
 
 var CLIENT_APP = []string{"website", "mobile", "android", "ios"}
